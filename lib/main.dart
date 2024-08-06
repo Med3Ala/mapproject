b0c89f089108dc9a5d424089319c88f0da80be94
ala.mouhamed <ala.mouhamed@tunav.com>
2024-08-06 22:42:02 +0100

    setting up a custom style from maptiler

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:flutter/services.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
-import 'package:maplibre_gl/maplibre_gl.dart';
+import 'package:vector_map_tiles/vector_map_tiles.dart';
 
 const apiKey = "FHh9gFfl6vBRSuj5eWXi";
-const styleUrl = "https://api.maptiler.com/maps/streets-v2/style.json";
+const styleUrl = "https://api.maptiler.com/maps/f6a7b4d0-b349-4cb0-843f-9d70be0cdb5c/style.json";
 
 void main() {
   runApp(const MyApp());
@@ -32,23 +35,61 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final mapController = MapController();
 
+      Future<Style> _readStyle() => StyleReader(
+          uri: "${styleUrl}?key=${apiKey}",
+          // ignore: undefined_identifier
+          apiKey: apiKey)
+      .read();
+
+      // Future<Style> _loadStyle() async {
+      //   // final jsonString = await rootBundle.loadString('./style.json');
+      //   // final jsonData = jsonDecode(jsonString) as Map<String, dynamic>;
+      //   return StyleReader(
+      //     uri: "./style.json",
+      //     apiKey: apiKey,
+      //   ).read();
+      // }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        children: [  
+        children: [
           Positioned(
             top: 0,
             left: 0,
             height: MediaQuery.of(context).size.height,
             width: MediaQuery.of(context).size.width,
-            child: MaplibreMap(
-              styleString: "$styleUrl?key=$apiKey",
-              myLocationEnabled: false,
-              initialCameraPosition: const CameraPosition(target: LatLng(36, 10)),
-              trackCameraPosition: true,
+            child: FutureBuilder<Style>(
+              future: _readStyle(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator()); // Or any loading indicator
+                } else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else {
+                  final style = snapshot.data!; // Access the fetched style
 
-          )),
+                  //final style = snapshot.data!;
+
+                  return FlutterMap(
+                    options: MapOptions(
+                      initialCenter: const LatLng(31.050478, -7.931633),
+                      initialZoom: 12.0,
+                    ),
+                    children: [
+                      VectorTileLayer(
+                        theme: style.theme,
+                        sprites: style.sprites,
+                        // tileOffset: TileOffset.mapbox, enable with mapbox
+                        tileProviders: style.providers,
+                      ),
+                    ],
+                  );
+                }
+              },
+            ),
+          ),
           Positioned(
             bottom: 0,
             left: 0,
@@ -66,9 +107,9 @@ class _MyHomePageState extends State<MyHomePage> {
                 // Handle bottom navigation bar taps
               },
             ),
-          )
+          ),
         ],
-      )
+        )
       
       
     );
